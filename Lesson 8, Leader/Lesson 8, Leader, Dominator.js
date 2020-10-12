7522204f75b84db98da6f8cf7af0cdf2516b5a69
user <fernando.zuher@gmail.com>
2020-10-11 23:49:40 -0300

    l8

--- Lesson 8, Leader/Lesson 8, Leader, Dominator.js
+++ Lesson 8, Leader/Lesson 8, Leader, Dominator.js
@@ -1,21 +1,20 @@
-// Function mode adapted from: http://rosettacode.org/wiki/Averages/Mode#JavaScript
+// Function Mode adapted from: http://rosettacode.org/wiki/Averages/Mode#JavaScript
 
-function mode(ary) {
-    let [counter, mode, max] = [{}, A[0], 0]
-    for (let i in ary) {
-        if (!(ary[i] in counter))
-            counter[ary[i]] = 0
-        counter[ary[i]]++
+function Mode(ary) {
+    let [counter, mode, max, index, i] = [{}, ary[0], 0, 0, 0]
+    for (let num of ary) {
+        if (!(num in counter))
+            counter[num] = 0
+        counter[num]++
 
-        if (counter[ary[i]] > max) {
-            max = counter[ary[i]]
-            mode = ary[i]
-        }
+        if (counter[num] > max)
+            [mode, max, index] = [num, counter[num], i]
+        i++
     }
-    return mode
+    return [max, index]
 }
 
 function solution(A) {
-    const mode = mode(A)
-    return mode && A.filter(x => x === mode).length > (A.length / 2) ? A.indexOf(mode) : -1
+    const mode = Mode(A)
+    return mode[0] > (A.length / 2) ? mode[1] : -1
 }
